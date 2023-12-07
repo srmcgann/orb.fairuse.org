7d7a7c2d90c46669488c4fc42cfb1e68afb134d5
scott mcgann <s.r.mcgann@hotmail.com>
2023-12-07 20:30:26 +0000

    initial commit

---
+++ 4/setDomain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter the new domain..."
+read -r newdomain
+
+echo "enter the new database USER name"
+read -r newdbuser
+
+echo "enter the new database name"
+read -r newdb
+
+php sd.php "$newdomain" "$newdbuser" "$newdb";
