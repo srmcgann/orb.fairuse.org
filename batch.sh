8f242a44f3b7b2104cdc029fc22910ed9d563961
scott mcgann <s.r.mcgann@hotmail.com>
2023-12-07 21:31:16 +0000

    sync

---
+++ batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./setDomainBatch.sh fishable-searches.000webhostapp.com id21284549_user id21284549_videodemos2
+mv *.zip ..
+./setDomainBatch.sh orbs2.000webhostapp.com id21552617_user id21552617_orbs2
+mv *.zip ..
+./setDomainBatch.sh orbs3.000webhostapp.com id21553412_user id21553412_orbs3
+mv *.zip ..
+./setDomainBatch.sh orbs4.000webhostapp.com id21583283_user id21583283_orbs4
+mv *.zip ..
+./setDomainBatch.sh efx2.000webhostapp.com id21601194_user id21601194_efx2
+mv *.zip ..
+./setDomainBatch.sh efx3.000webhostapp.com id21601862_user id21601862_efx3
+mv *.zip ..
+./setDomainBatch.sh warpspeed.000webhostapp.com id21607252_user id21607252_warpspeed
+mv *.zip ..
+#./setDomainBatch orbsools.000webhostapp.com 1 1
+#mv *.zip ..
