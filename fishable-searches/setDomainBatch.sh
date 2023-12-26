ccbbffbcccd5ac113fb4d698f39d2e7b11506daf
scott mcgann <s.r.mcgann@hotmail.com>
2023-12-26 22:27:56 +0000

    sync

---
+++ fishable-searches/setDomainBatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#echo "enter the new domain..."
+#read -r newdomain
+
+#echo "enter the new database USER name"
+#read -r newdbuser
+
+#echo "enter the new database name"
+#read -r newdb
+
+#php sd.php "$newdomain" "$newdbuser" "$newdb";
+php sd.php "$1" "$2" "$3" "$4";
